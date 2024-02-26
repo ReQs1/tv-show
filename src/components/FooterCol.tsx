e47b1d7f4dfbf264d3080447a450cb4633ffca6f
Req <snogedirl@gmail.com>
2024-02-26 23:15:20 +0100

    Refactor FooterCol and update movie page background image

--- src/components/FooterCol.tsx
+++ src/components/FooterCol.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/lib/utils";
+
 type Props = {
   title: string;
   links: any[];
@@ -5,28 +7,24 @@ type Props = {
 };
 
 function FooterCol({ title, links, variant }: Props) {
-  if (variant === "socials")
-    return (
-      <div className="flex flex-col items-center flex-1 sm:items-start">
-        <h5 className="mb-4 text-sm text-yellow-400 uppercase md:text-base md:mb-6">
-          Follow Us
-        </h5>
-        <ul className="flex flex-col items-center gap-5 text-sm font-semibold md:flex-row md:gap-8">
-          {links.map((link, index) => (
-            <li key={index} className="cursor-pointer">
-              {link}
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-
   return (
-    <div className="flex-1">
+    <div
+      className={cn("flex-1", {
+        ["flex flex-col items-center flex-1 sm:items-start"]:
+          variant === "socials",
+      })}
+    >
       <h5 className="mb-4 text-sm text-yellow-400 uppercase md:text-base md:mb-6">
         {title}
       </h5>
-      <ul className="space-y-5 text-sm font-semibold md:text-lg lg:space-y-6">
+      <ul
+        className={cn("text-sm", {
+          ["flex flex-col items-center gap-5 font-semibold md:flex-row md:gap-8"]:
+            variant === "socials",
+          ["space-y-5 font-semibold md:text-lg lg:space-y-6"]:
+            variant !== "socials",
+        })}
+      >
         {links.map((link, index) => (
           <li key={index} className="cursor-pointer">
             {link}
--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -57,7 +57,7 @@ function MoviePage() {
       <header
         className="px-4 py-8 text-white bg-[top_center] bg-no-repeat bg-cover shadow-lg sm:px-8 md:px-10 xl:px-12 md:py-12 lg:py-16 xl:py-20"
         style={{
-          backgroundImage: `linear-gradient(180deg, #00000026 0%, #000000 100%), url('https://image.tmdb.org/t/p/original${backdrop_path}')`,
+          backgroundImage: `linear-gradient(180deg, #0000004d 0%, #000000 100%), url('https://image.tmdb.org/t/p/original${backdrop_path}')`,
         }}
       >
         {/* poster */}
