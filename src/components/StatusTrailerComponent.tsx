dd28304113fc12f58119429e382a770e1560878a
Req <snogedirl@gmail.com>
2024-02-26 18:07:37 +0100

    Rewrote MoviePage to have 1 div for status div

--- src/components/StatusTrailerComponent.tsx
+++ src/components/StatusTrailerComponent.tsx
@@ -3,56 +3,30 @@ import StatusButtons from "./StatusButtons";
 type Props = {
   popularity: number;
   status: string;
-  variant: "mobile" | "desktop";
 };
 
-function StatusTrailerComponent({ popularity, status, variant }: Props) {
-  if (variant === "mobile")
-    return (
-      // status
-      <div className="flex flex-col items-center mt-10 xl:hidden">
-        <div className="flex flex-col items-center gap-2 sm:flex-row">
-          <div className="w-72 sm:w-60 px-4 py-3 space-y-2 flex flex-col justify-center items-center text-white bg-black rounded-md h-[108px]">
-            <p>Status</p>
-            <p className="text-lg font-bold">{status}</p>
-          </div>
-
-          {/* popularity */}
-
-          <div className="w-72 sm:w-60 px-4 py-3 space-y-2 flex items-center justify-center flex-col text-white bg-black rounded-md h-[108px]">
-            <p>TheMovieDB Popularity</p>
-            <p className="text-lg font-bold">{Math.floor(popularity)}</p>
-          </div>
+function StatusTrailerComponent({ popularity, status }: Props) {
+  return (
+    <div className="flex flex-col items-center mt-10 ">
+      <div className="flex flex-col items-center gap-6 sm:flex-row xl:flex-col">
+        <div className="w-72 sm:w-60 xl:w-72 px-4 py-3 space-y-2 flex flex-col justify-center items-center text-white bg-black rounded-md h-[108px]">
+          <p>Status</p>
+          <p className="text-lg font-bold">{status}</p>
         </div>
 
-        <div className="space-y-4 mt-14">
-          <StatusButtons />
+        {/* popularity */}
+
+        <div className="w-72 sm:w-60 xl:w-72 px-4 py-3 space-y-2 flex items-center justify-center flex-col text-white bg-black rounded-md h-[108px]">
+          <p>TheMovieDB Popularity</p>
+          <p className="text-lg font-bold">{Math.floor(popularity)}</p>
         </div>
       </div>
-    );
 
-  if (variant === "desktop")
-    return (
-      <div className="hidden xl:block">
-        <div className="flex flex-col items-center">
-          <div className="py-3 space-y-1 text-center bg-black rounded-md w-72">
-            <p className="text-xl">Status</p>
-            <p className="text-2xl font-bold">{status}</p>
-          </div>
-
-          {/* popularity  */}
-
-          <div className="py-3 mt-8 space-y-1 text-center bg-black rounded-md w-72">
-            <p className="text-xl">TheMovieDB Popularity</p>
-            <p className="text-2xl font-bold">{Math.floor(popularity)}</p>
-          </div>
-
-          <div className="mt-16 space-y-5 ">
-            <StatusButtons />
-          </div>
-        </div>
+      <div className="space-y-4 mt-14">
+        <StatusButtons />
       </div>
-    );
+    </div>
+  );
 }
 
 export default StatusTrailerComponent;
--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -55,58 +55,53 @@ function MoviePage() {
   return (
     <div>
       <header
-        className="px-4 py-8 text-white bg-[top_center] bg-no-repeat bg-cover shadow-lg sm:px-8 md:px-10 xl:px-20 md:py-12 lg:py-16 xl:py-20"
+        className="px-4 py-8 text-white bg-[top_center] bg-no-repeat bg-cover shadow-lg sm:px-8 md:px-10 xl:px-12 md:py-12 lg:py-16 xl:py-20 "
         style={{
           backgroundImage: `linear-gradient(180deg, rgba(0,212,255,0) 0%, #000000 100%), url('https://image.tmdb.org/t/p/original${backdrop_path}')`,
         }}
       >
         {/* poster */}
+        <div className="flex flex-col xl:flex-row">
+          <div className="flex flex-1 gap-4 md:gap-10 sm:gap-6">
+            <div className="max-w-40 sm:max-w-48 lg:max-w-56 xl:max-w-72">
+              <img
+                className="rounded-lg lg:w-full"
+                src={
+                  poster_path
+                    ? `https://image.tmdb.org/t/p/w500${poster_path}`
+                    : "https://placehold.co/500x750?text=No+Movie+Poster"
+                }
+                alt={`${truncate(name) || truncate(title)}`}
+                loading="lazy"
+              />
+            </div>
 
-        <div className="flex gap-4 md:gap-10 sm:gap-6">
-          <div className="max-w-36 sm:max-w-48 lg:max-w-56 xl:max-w-72">
-            <img
-              className="rounded-lg lg:w-full"
-              src={
-                poster_path
-                  ? `https://image.tmdb.org/t/p/w500${poster_path}`
-                  : "https://placehold.co/500x750?text=No+Movie+Poster"
-              }
-              alt={`${truncate(name) || truncate(title)}`}
-              loading="lazy"
-            />
+            <div className="flex flex-col flex-1 gap-2">
+              <h1 className="text-2xl font-bold md:text-3xl">
+                {name || title}
+              </h1>
+
+              <StarRatingComponent
+                vote_average={vote_average}
+                className="flex items-center gap-1"
+              />
+
+              <GenreMapComponent genres={genres} />
+
+              <RuntimeAndSeasonsComponent runtime={runtime} />
+
+              <RuntimeAndSeasonsComponent
+                number_of_seasons={number_of_seasons}
+              />
+
+              <OverviewComponent overview={overview} variant="desktop" />
+            </div>
           </div>
 
-          <div className="flex flex-col flex-1 gap-2">
-            <h1 className="text-2xl font-bold md:text-3xl">{name || title}</h1>
+          <OverviewComponent overview={overview} variant="mobile" />
 
-            <StarRatingComponent
-              vote_average={vote_average}
-              className="flex items-center gap-1"
-            />
-
-            <GenreMapComponent genres={genres} />
-
-            <RuntimeAndSeasonsComponent runtime={runtime} />
-
-            <RuntimeAndSeasonsComponent number_of_seasons={number_of_seasons} />
-
-            <OverviewComponent overview={overview} variant="desktop" />
-          </div>
-
-          <StatusTrailerComponent
-            status={status}
-            popularity={popularity}
-            variant="desktop"
-          />
+          <StatusTrailerComponent popularity={popularity} status={status} />
         </div>
-
-        <OverviewComponent overview={overview} variant="mobile" />
-
-        <StatusTrailerComponent
-          status={status}
-          popularity={popularity}
-          variant="mobile"
-        />
       </header>
     </div>
   );
